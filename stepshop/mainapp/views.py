6a2222736f2d81cfd51affb23a078f606fc9ad68
Viktor <viktorg1105@gmail.com>
2022-06-01 17:47:11 +0600

    add: lesson_04

--- stepshop/mainapp/views.py
+++ stepshop/mainapp/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
+links_menu = [
+    {'href': 'index', 'name': 'Домой', 'route': ''},
+    {'href': 'products:index', 'name': 'Продукты', 'route': 'products/'},
+    {'href': 'about', 'name': 'О нас', 'route': 'about/'},
+    {'href': 'contacts', 'name': 'Контакты', 'route': 'contacts/'},
+]
 
 def products(request):
     title = 'Продукты'
     context = {
-        'title' : title,
+        'title': title,
+        'links_menu': links_menu,
     }
 
     return render(request, "products.html", context)
--- stepshop/stepshop/views.py
+++ stepshop/stepshop/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 
+links_menu = [
+    {'href': 'index', 'name': 'Домой', 'route': ''},
+    {'href': 'products:index', 'name': 'Продукты', 'route': 'products/'},
+    {'href': 'about', 'name': 'О нас', 'route': 'about/'},
+    {'href': 'contacts', 'name': 'Контакты', 'route': 'contacts/'},
+]
 
 def index(request):
     title = 'Главная страница'
+
+
     context = {
         'title': title,
+        'links_menu': links_menu,
     }
     return render(request, 'index.html', context)
 
@@ -14,6 +23,7 @@ def about(request):
     title = 'О нас'
     context = {
         'title': title,
+        'links_menu': links_menu,
     }
     return render(request, "about.html", context)
 
@@ -24,5 +34,6 @@ def contacts(request):
     title = 'Контакты'
     context = {
         'title': title,
+        'links_menu': links_menu,
     }
     return render(request, 'contact.html', context)
